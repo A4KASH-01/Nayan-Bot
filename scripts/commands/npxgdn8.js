5641fb8f356a4411cb258982b3af96c21966e404
A4KASH-01 <161235562+A4KASH-01@users.noreply.github.com>
2025-04-04 14:15:26 +0600

    Update npxgdn8.js

--- scripts/commands/npxgdn8.js
+++ scripts/commands/npxgdn8.js
@@ -19,7 +19,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	if (body.indexOf("Good night")==0 || body.indexOf("Gd n8")==0 || body.indexOf("good night")==0 || body.indexOf("gd n8")==0) {
 		var msg = {
 				body: "ও্ঁকে্ঁ সো্ঁনা গু্ঁড্ঁ না্ঁই্ঁট্ঁ 🌚",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/hanhtinh/goodnight.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😁", event.messageID, (err) => {}, true)
